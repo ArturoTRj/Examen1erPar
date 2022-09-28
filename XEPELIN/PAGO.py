4b2a3d44143c8ae8f56343f06d3b10c4310770d6
Arturo <atrejoj001@alumno.uaemex.mx>
2022-09-27 21:26:58 -0500

    agregados los programas

---
+++ XEPELIN/PAGO.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from Factura import Factura
+
+class PAGO(Factura):
+    def __init__(self):
+        self.id pago = None
+        self.numero de tarjeta. = None
+
+    def Agregar_Tarjeta(self, ):"
+
+        print("AGREGAR UNA TARJETA ")
+        pass
+
+    def Borrar_tarjeta(self, ):
+        print("Borrar tarjeta "))
+        pass
+
+    def moficar_tarjeta(self, ):
+        print("Modificar UNA TARJETA ")
+        pass
